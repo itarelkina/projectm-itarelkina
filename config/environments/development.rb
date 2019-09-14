7e02a8efa4a3f21ad5a6a9cd5c40b20a5dfe1af7
Irina Tarelkina <itarelkina@yahoo.ca>
2019-09-14 18:12:47 +0000

    added application.erb.html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   
   # devise requirement 
   
-mconfig.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
